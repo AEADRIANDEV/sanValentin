9daf31e5464cb66fa576fa90500787cf954525ec
AEADRIANDEV <adrian.estrada.dora@gmail.com>
2024-02-06 15:55:21 -0500

    fin primera parte

---
+++ script.js
@@ -0,0 +1,69 @@
+//"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES=10;
+
+let play=true;
+let noCount=0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+    if (play) {
+      noCount++;
+      const imageIndex = Math.min(noCount, MAX_IMAGES);
+      changeImage(imageIndex);
+      resizeYesButton();
+      updateNoButtonText();
+      if (noCount === MAX_IMAGES) {
+        noButton.classList.add("hidden");
+        play = false;
+      }
+    }
+  });
+
+function handleYesClick() {
+    titleElement.innerHTML = "¡¡Siiuuuu!! :3";
+    buttonsContainer.classList.add("hidden");
+    changeImage("yes");
+}
+
+function resizeYesButton() {
+    const computedStyle = window.getComputedStyle(yesButton);
+    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+    const newFontSize = fontSize * 1.2;
+  
+    yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+    const messages = [
+      "No",
+      "Estas Segura?",
+      "...",
+      "Esto no es Chestoso",
+      "¡Ya! modo serio SI o Si",
+      "Veraaaaaaas",
+      "Cuidao",
+      "Deberitas no?",
+      "Grosera",
+      "Toma por mala",
+      "Ya no puedes decir que no jejex"
+    ];
+  
+    const messageIndex = Math.min(noCount, messages.length - 1);
+    return messages[messageIndex];
+}
+
+function changeImage(image) {
+    catImg.src = `img2/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+    titleElement.innerHTML = generateMessage(noCount);
+}
